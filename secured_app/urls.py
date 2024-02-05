91384790d601ae58bb4649575e244cdb7744267f
amirul_islam <amirul.islam@vivacomsolutions.com>
2024-02-05 14:07:53 +0600

    registration form done

--- secured_app/urls.py
+++ secured_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name=''),
     path('register', views.register, name='register'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
