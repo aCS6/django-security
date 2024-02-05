03a332e2d370e82bf98b872ee18dcf4081405c7d
amirul_islam <amirul.islam@vivacomsolutions.com>
2024-02-05 14:07:53 +0600

    registration form done

--- secured_app/views.py
+++ secured_app/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import CreateUserForm
 
 
 def home(request):
     return render(request, 'index.html')
 
 def register(request):
-    return render(request, 'register.html')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('')
+        
+    context = {'form': form}
+    return render(request, 'register.html', context=context)
 
 
 def dashboard(request):
