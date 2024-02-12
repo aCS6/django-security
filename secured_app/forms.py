dd84e0c86e3d8f09c8b38483a6568bf366d79f93
amirul_islam <amirul.islam@vivacomsolutions.com>
2024-02-12 14:22:23 +0600

    favicon added

--- secured_app/forms.py
+++ secured_app/forms.py
@@ -1,8 +1,9 @@
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django_recaptcha.fields import ReCaptchaField
 
-# recaptcha
+from django import forms
 
 class CreateUserForm(UserCreationForm):
     class Meta:
@@ -15,3 +16,15 @@ class CreateUserForm(UserCreationForm):
         ]
     
     captcha = ReCaptchaField()
+    
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is invalid!")
+        
+        if len(email) >= 300:
+            raise forms.ValidationError("Email is too long!")
